d3d08647bd246a3306b77807da7cabd22327c7f4
VictriD <weihao.jiang@sjtu.edu.cn>
2020-04-19 19:55:58 +0800

    update: lot of errors

--- deque/deque.hpp
+++ deque/deque.hpp
@@ -546,6 +546,8 @@ struct d_index {
 
 template <class T>
 class deque {
+    friend class deque;
+
 private:
     //using a vector to contain all data.
     vector<vector<T>> container;
@@ -567,124 +569,254 @@ private:
         ans.subpos = pos - sp;
         return ans;
     };
+    // the function does what you think.
+    void split(typename vector<vector<T>>::iterator it) {
+        size_t _s = (*it).size() / 2;
+        vector<T> back(_s);
+        container.insert(it + 1, back(_s));
+        for (auto ps = (*it).begin() + ((*it).size() - _s); ps != (*it).end(); ps = (*it).erase(ps))
+            *(it + 1).push_back(*ps);
+        return;
+    }
+    // merge it and it+1 UNSAFE
+    void merge(typename vector<vector<T>>::iterator it) {
+        for (auto ps = *(it + 1).begin(); ps != *(it + 1).end(); ps = *(it + 1).erase(ps))
+            (*it).push_back(*ps);
+        container.erase(it + 1);
+        return;
+    }
+    void remove_empty_block() {
+        for (auto ps = container.begin(); ps != container.end();) {
+            if ((*ps).size() == 0) {
+                container.erase(ps);
+                continue;
+            } else {
+                ps++;
+            }
+        }
+        return;
+    }
+    //TODO fit function
+    void fit(){};
 
 public:
     class const_iterator;
     class iterator {
     private:
-        /**
-         * TODO add data members
-         *   just add whatever you want.
-         */
     public:
+        //* i0  here can be optimized with block-subposition
+        deque<T>* deq;
+        size_t delta;
+        iterator(deque<T>* deqs, size_t delta) : deq(deqs), delta(delta){};
+        iterator() : deq(nullptr), delta(0){};
         /**
          * return a new iterator which pointer n-next elements
          *   even if there are not enough elements, the behaviour is **undefined**.
          * as well as operator-
          */
         iterator operator+(const int& n) const {
-            //TODO
+            return iterator(this, delta + n);
         }
         iterator operator-(const int& n) const {
-            //TODO
+            return iterator(this, delta - n);
         }
         // return th distance between two iterator,
         // if these two iterators points to different vectors, throw invaild_iterator.
         int operator-(const iterator& rhs) const {
-            //TODO
+            if (rhs.deq != deq)
+                throw(invalid_iterator());
+            return delta - rhs.delta;
         }
         iterator& operator+=(const int& n) {
-            //TODO
+            delta += n;
+            return *this;
         }
         iterator& operator-=(const int& n) {
-            //TODO
+            delta -= n;
+            return *this;
         }
-        /**
-         * TODO iter++
-         */
-        iterator operator++(int) {}
+        iterator operator++(int) {
+            auto ps = *this;
+            ++delta;
+            return ps;
+        }
         /**
          * TODO ++iter
          */
-        iterator& operator++() {}
+        iterator& operator++() {
+            ++delta;
+            return *this;
+        }
         /**
          * TODO iter--
          */
-        iterator operator--(int) {}
+        iterator operator--(int) {
+            auto ps = *this;
+            --delta;
+            return ps;
+        }
         /**
          * TODO --iter
          */
-        iterator& operator--() {}
+        iterator& operator--() {
+            --delta;
+            return *this;
+        }
         /**
          * TODO *it
          */
-        T& operator*() const {}
+        T& operator*() const {
+            return deq[delta];
+        }
         /**
          * TODO it->field
          */
-        T* operator->() const noexcept {}
+        T* operator->() const noexcept {
+            return &deq[delta];
+        }
         /**
          * a operator to check whether two iterators are same (pointing to the same memory).
          */
-        bool operator==(const iterator& rhs) const {}
-        bool operator==(const const_iterator& rhs) const {}
+        bool operator==(const iterator& rhs) const {
+            return (deq == rhs.deq && delta == rhs.delta);
+        }
+        bool operator==(const const_iterator& rhs) const {
+            return (deq == rhs.deq && delta == rhs.delta);
+        }
         /**
          * some other operator for iterator.
          */
-        bool operator!=(const iterator& rhs) const {}
-        bool operator!=(const const_iterator& rhs) const {}
+        bool operator!=(const iterator& rhs) const {
+            return !(deq == rhs.deq && delta == rhs.delta);
+        }
+        bool operator!=(const const_iterator& rhs) const {
+            return !(deq == rhs.deq && delta == rhs.delta);
+        }
     };
     class const_iterator {
-        // it should has similar member method as iterator.
-        //  and it should be able to construct from an iterator.
     private:
-        // data members.
     public:
-        const_iterator() {
-            // TODO
+        deque<T>* deq;
+        size_t delta;
+        const_iterator(deque<T>* deqs, size_t delta) : deq(deqs), delta(delta){};
+        const_iterator() : deq(nullptr), delta(0){};
+        const_iterator(const const_iterator& other) : deq(other.deq), delta(other.delta){};
+        const_iterator(const iterator& other) : deq(other.deq), delta(other.delta){};
+        const_iterator operator+(const int& n) const {
+            return const_iterator(this, delta + n);
         }
-        const_iterator(const const_iterator& other) {
-            // TODO
+        const_iterator operator-(const int& n) const {
+            return const_iterator(this, delta - n);
         }
-        const_iterator(const iterator& other) {
-            // TODO
+        // return th distance between two iterator,
+        // if these two iterators points to different vectors, throw invaild_iterator.
+        int operator-(const const_iterator& rhs) const {
+            if (rhs.deq != deq)
+                throw(invalid_iterator());
+            return delta - rhs.delta;
         }
-        // And other methods in iterator.
-        // And other methods in iterator.
-        // And other methods in iterator.
+        const_iterator& operator+=(const int& n) {
+            delta += n;
+            return *this;
+        }
+        const_iterator& operator-=(const int& n) {
+            delta -= n;
+            return *this;
+        }
+        const_iterator operator++(int) {
+            auto ps = *this;
+            ++delta;
+            return ps;
+        }
+        const_iterator& operator++() {
+            ++delta;
+            return *this;
+        }
+        const_iterator operator--(int) {
+            auto ps = *this;
+            --delta;
+            return ps;
+        }
+        const_iterator& operator--() {
+            --delta;
+            return *this;
+        }
+        const T& operator*() const {
+            return deq[delta];
+        }
+        const T* operator->() const noexcept {
+            return &deq[delta];
+        }
+        bool operator==(const iterator& rhs) const {
+            return (deq == rhs.deq && delta == rhs.delta);
+        }
+        bool operator==(const const_iterator& rhs) const {
+            return (deq == rhs.deq && delta == rhs.delta);
+        }
+        bool operator!=(const iterator& rhs) const {
+            return !(deq == rhs.deq && delta == rhs.delta);
+        }
+        bool operator!=(const const_iterator& rhs) const {
+            return !(deq == rhs.deq && delta == rhs.delta);
+        }
     };
 
-    /**
-     * TODO Constructors
-     */
-    deque() {}
-    deque(const deque& other) {}
-    /**
-     * TODO Deconstructor
-     */
+    deque() : d_size(0) {}
+    deque(const deque& other) {
+        container = other.container;
+        d_size    = other.d_size;
+        fit();
+    }
     ~deque() {}
-    /**
-     * TODO assignment operator
-     */
-    deque& operator=(const deque& other) {}
+    deque& operator=(const deque& other) {
+        if (*other == this)
+            return *this;
+        container = other.container;
+        d_size    = other.d_size;
+        fit();
+    }
     /**
      * access specified element with bounds checking
      * throw index_out_of_bound if out of bound.
      */
-    T& at(const size_t& pos) {}
-    const T& at(const size_t& pos) const {}
-    T& operator[](const size_t& pos) {}
-    const T& operator[](const size_t& pos) const {}
+    T& at(const size_t& pos) {
+        if (pos >= d_size)
+            throw(index_out_of_bound());
+        d_index d = query(pos);
+        return container[d.block][d.subpos];
+    }
+    const T& at(const size_t& pos) const {
+        if (pos >= d_size)
+            throw(index_out_of_bound());
+        d_index d = query(pos);
+        return container[d.block][d.subpos];
+    }
+    T& operator[](const size_t& pos) {
+        return at(pos);
+    }
+    const T& operator[](const size_t& pos) const {
+        return at(pos);
+    }
     /**
      * access the first element
      * throw container_is_empty when the container is empty.
      */
-    const T& front() const {}
+    const T& front() const {
+        if (d_size == 0)
+            throw(container_is_empty());
+        remove_empty_block();
+        return container[0][0];
+    }
     /**
      * access the last element
      * throw container_is_empty when the container is empty.
      */
-    const T& back() const {}
+    const T& back() const {
+        if (d_size == 0)
+            throw(container_is_empty());
+        remove_empty_block();
+        return container.back().back();
+    }
     /**
      * returns an iterator to the beginning.
      */
@@ -698,47 +830,93 @@ public:
     /**
      * checks whether the container is empty.
      */
-    bool empty() const {}
+    bool empty() const {
+        return !(d_size);
+    }
     /**
      * returns the number of elements
      */
-    size_t size() const {}
+    size_t size() const {
+        return d_size;
+    }
     /**
      * clears the contents
      */
-    void clear() {}
+    void clear() {
+        if (d_size == 0)
+            return;
+        container.clear;
+        d_size = 0;
+        return;
+    }
     /**
      * inserts elements at the specified locat on in the container.
      * inserts value before pos
      * returns an iterator pointing to the inserted value
      *     throw if the iterator is invalid or it point to a wrong place.
      */
-    iterator insert(iterator pos, const T& value) {}
+    iterator insert(iterator pos, const T& value) {
+        if (pos.deq != this || pos.delta > d_size || pos.delta < 0)
+            throw(invalid_iterator());
+        if (pos.delta == d_size) {
+            push_back(value);
+            return pos;
+        }
+        //* optimize i0
+        d_index d = query(pos.delta);
+        container[d.block].insert(d.subpos, value);
+        return pos;
+    }
     /**
      * removes specified element at pos.
      * removes the element at pos.
      * returns an iterator pointing to the following element, if pos pointing to the last element, end() will be returned.
      * throw if the container is empty, the iterator is invalid or it points to a wrong place.
      */
-    iterator erase(iterator pos) {}
+    iterator erase(iterator pos) {
+        if (d_size == 0)
+            throw(container_is_empty());
+        if (pos.deq != this || pos.delta > d_size || pos.delta < 0)
+            throw(invalid_iterator());
+        //* optimize i0
+        d_index d = query(pos.delta);
+        container[d.block].erase(d.subpos);
+        //* d1 whether it should clear empty here?
+        return pos;
+    }
     /**
      * adds an element to the end
      */
-    void push_back(const T& value) {}
+    void push_back(const T& value) {
+        container.back().push_back(value);
+    }
     /**
      * removes the last element
      *     throw when the container is empty.
      */
-    void pop_back() {}
+    void pop_back() {
+        if (d_size == 0)
+            throw(container_is_empty());
+        remove_empty_block();
+        container.back().pop_back();
+    }
     /**
      * inserts an element to the beginning.
      */
-    void push_front(const T& value) {}
+    void push_front(const T& value) {
+        //* d2 split within
+        container.front().insert(0, value);
+    }
     /**
      * removes the first element.
      *     throw when the container is empty.
      */
-    void pop_front() {}
+    void pop_front() {
+        if (d_size == 0)
+            throw(container_is_empty());
+        remove_empty_block();
+        container.front().erase(0);
+    }
 };
 
 } // namespace sjtu
